37093e1c317232eee85dbb334af78a67c9bf6d09
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-07-26 15:16:32 -0700

    adds update functionality for words

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/dictionary')
 also_reload('./lib/**/*.rb')
+also_reload('./public/*.css')
 
 get ('/') do
   erb :default
@@ -17,16 +18,38 @@ get ('/words') do
 end
 
 get ('/words/:word')do
-
     @word = params[:word]
     @definitions = nil
     #this is a fucked up solution for multiple definitions
     #¯\_(ツ)_/¯
     match = Dictionary::Word.all.select{|k,v| v == @word}
     @definitions = match
-
     erb :word_page
+end
 
+get ('/delete/:word')do
+    Dictionary::Word.delete(params[:word])
+    redirect to ("/")
+end
+
+get('/update/:word')do
+    @word = params[:word]
+    @definitions = nil
+    #this is a fucked up solution for multiple definitions
+    #¯\_(ツ)_/¯
+    match = Dictionary::Word.all.select{|k,v| v == @word}
+    @definitions = match
+    erb :update_word
+end
+
+post ('/update_word/:word') do
+  Dictionary::Word.delete(params[:word])
+  new_defs = params.select{|k,v| k =~ /new_def_*/}
+  new_defs.each_value do |value|
+    word = Dictionary::Word.new({:name => params[:word_name_update], :def => value})
+    word.save
+  end
+  redirect to ("/")
 end
 
 post ('/add_word') do
